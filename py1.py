30c8e4019e32ee2111ed958cc80f36b16b015e75
meldr1s <vladvip8753@gmail.com>
2025-01-16 21:00:09 +0200

    1Les Fin

--- py1.py
+++ py1.py
@@ -104,3 +104,83 @@ def function(n):
 
 result = function(N)  
 # print(f"The sum of the squares of numbers from 1 to {N} is {result}")
+
+
+first_name = "John"
+last_name = "Doe"
+
+
+def get_fullname(first_name, last_name):
+    full = first_name + " " + last_name
+    return full
+
+fullname = get_fullname(first_name, last_name)
+# print(fullname)
+
+
+first_name = "John"
+last_name = "Doe"
+
+
+def get_initials(first_name, last_name):
+    name = str(last_name) + " " + first_name[0] + "."
+    return name
+
+formatted_name  = get_initials(first_name, last_name)
+# print(formatted_name)
+
+
+first = "Python"
+second = "python"
+
+
+def compare(first, second):
+    if first.upper() == second.upper():
+        return True
+    else:
+        return False
+    
+
+
+result = compare(first, second)
+# print(result)
+
+
+text = "Hello, world! Welcome to the world of Python."
+
+position = text.find("world")
+# print(position)
+
+updated_text = text.replace("world", "planet")
+# print(updated_text)
+
+
+product_name = "Coffee Maker"
+product_price = 7500.50
+product_quantity = 15
+
+
+def format_product_info(product_name, product_price, product_quantity):
+    coffe = f"Product: {product_name}, Price: {product_price} UAH, Quantity: {product_quantity} pcs."
+    return coffe
+
+product_info = format_product_info(product_name, product_price, product_quantity)
+# print(product_info)
+
+
+class Car:
+    vehicle_type = "Automobile"
+
+    def __init__(self, make, model, year):
+        self.make = make
+        self.model = model
+        self.year = year
+
+    def get_info(self):
+        return f"{self.year} {self.make} {self.model}, Type: {Car.vehicle_type}"
+    
+car_ford = Car("Ford", "Mustang", 2022)
+car_toyota = Car("Toyota", "Corolla", 2021)
+print(car_ford.get_info())
+print(car_toyota.get_info())
+
