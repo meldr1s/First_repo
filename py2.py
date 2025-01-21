1e975507bd641f611ad137ae381b6a1c1f300e9c
meldr1s <vladvip8753@gmail.com>
2025-01-21 20:06:38 +0200

    up

---
+++ py2.py
@@ -0,0 +1,18 @@
+# ім_я = input("Введіть ваше ім'я: ")
+
+# вітання = f"Привіт, {ім_я}!"
+
+# print(вітання)
+
+
+rate = 1.68
+value_day = 15
+value_night = 10
+payment = (rate * value_day) + ((rate / 2) *value_night)
+# print(payment)
+
+
+first_name = "Vlad"
+last_name = "P"
+full_name = f"{first_name} {last_name}"
+print(full_name)
