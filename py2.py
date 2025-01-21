0b2af5f0215850dec8fe50d5860305dbaf4b0abf
meldr1s <vladvip8753@gmail.com>
2025-01-21 21:15:32 +0200

    flpc1

--- py2.py
+++ py2.py
@@ -18,10 +18,44 @@ full_name = f"{first_name} {last_name}"
 # print(full_name)
 
 
-length = 2.75
-width = 1.75
-area = length * width
+length = "2.75"
+width = "1.75"
+area = float(length) * float(width)
 show = f"With width {width} and length {length} of the room, its area is equal to {area:.2f}"
 # print(show)
 
 
+# name = input("Your name? ")
+# email = input("Your email? ")
+# age = int(input("Your age? "))
+# height = float(input("Your height? "))
+# is_active = True
+# print(name, email, age, height, is_active)
+
+
+my_list = []
+my_list.insert(0, 2024)
+my_list.insert(1, "Python")
+my_list.insert(2, 3.12)
+# print(my_list)
+
+
+my_list = [2024, 3.12]
+some_data = ['Python']
+my_list.extend(some_data)
+my_list.insert(1, "Python")
+my_list.reverse()
+# print(my_list)
+
+
+data = {}
+data.update({"year": 2024, "lang": "Python", "version": 3.12})
+# print(data)
+
+
+cat = {}
+info = {"status": "vaccinated", "breed": True}
+cat.update({"nick": "Simon", "age": 7, "characteristics": ["лагідний", "кусається"]})
+age = cat.get("age")
+cat.update(info)
+# print(cat)
