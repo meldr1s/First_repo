287cb8394d92e86ccb1660f96ab44a352d9b19c7
meldr1s <vladvip8753@gmail.com>
2025-01-23 22:45:52 +0200

    l2

---
+++ py2.py
@@ -0,0 +1,102 @@
+# name = "Taras"
+# age = 22
+# has_driver_licence = True
+
+# if name and age >= 18 and has_driver_licence:
+#     print(f"User {name} can rent a car")
+
+
+# num = int(input("Введіть число: "))
+
+# length = len(str(num))
+
+# if length == 2 and num % 2 == 0:
+#     print("Парне двозначне число")
+# else:
+#     print("Ні")
+
+
+# is_next = None
+# num = int(input("Enter the number of points: "))
+# if num >= 83:
+#     is_next = True
+#     print("True")
+# else:
+#     is_next = False
+#     print("False")
+
+
+# work_experience = int(input("Enter your full work experience in years: "))
+# developer_type = None
+# if work_experience > 1 and work_experience <= 5:
+#     developer_type = "Middle"
+# elif work_experience <= 1:
+#     developer_type = "Junior"
+# else:
+#     developer_type = "Senior"
+# print(developer_type)
+
+
+# num = int(input("Enter a number: "))
+# result = None
+# if num > 0:
+#     if num % 2 == 1:
+#         result = "Positive odd number"
+#     else:
+#         result = "Positive even number"
+# elif num < 0:
+#     result = "Negative number"
+# else:
+#     result = "It is zero"
+# print(result)
+
+
+# num = int(input("Enter the integer (0 to 100): "))
+# sum = 0
+
+# if num < 0 or num > 100:
+#     print("Incorrect number")
+# else:
+#     while num > 0:
+#         sum += num
+#         num -= 1
+# print(sum)
+
+
+# message = "Never argue with stupid people, they will drag you down to their level and then beat you with experience."
+# search = "r"
+# result = 0
+# for m in message:
+#     if m == search:
+#         result += 1
+# print(result)
+
+
+# pool = 1000
+# quantity = int(input("Enter the number of mailings: "))
+# try:
+#     chunk = pool // quantity
+#     print(f"Each mailing will have {chunk} SMS.")
+# except ZeroDivisionError:
+#     print('Divide by zero completed!')
+
+
+# def greeting():
+#     print("Hello world!")
+# greeting()
+
+# username = str(input("Your username :"))
+# def invite_to_event(username):
+#     invite = f"Dear {username}, we have the honour to invite you to our event"
+#     return invite
+# invite = invite_to_event(username)
+# print(invite)
+
+
+def discount_price(price, discount):
+    def apply_discount():
+        
+        
+
+    
+    return price
