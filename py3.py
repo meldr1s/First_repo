149bb097c05bc23155e9bba20f4f7cd8d8594b0d
meldr1s <vladvip8753@gmail.com>
2025-01-28 21:55:00 +0200

    12

---
+++ py3.py
@@ -0,0 +1,47 @@
+# import datetime
+# print(datetime.datetime.now())
+
+# from datetime import datetime
+# current_datetime = datetime.now()
+# print(current_datetime.year)
+# print(current_datetime.month)
+# print(current_datetime.day)
+# print(current_datetime.hour)
+# print(current_datetime.minute)
+# print(current_datetime.second)
+# print(current_datetime.microsecond)
+# print(current_datetime.tzinfo)
+# print(current_datetime.date())
+# print(current_datetime.time())
+
+
+# import datetime
+
+# date_part = datetime.date(2023, 12, 14)
+# time_part = datetime.time(12, 30, 15)
+# specific_date = datetime.datetime(2020, 1, 7, 21, 32, 1)
+
+# combined_datetime = datetime.datetime.combine(date_part, time_part)
+# print(specific_date)
+# print(combined_datetime)
+
+
+# from datetime import datetime
+
+# now = datetime.now()
+# day_of_week = now.weekday()
+
+# print(f"Сьогодні: {day_of_week}") 
+
+
+# from datetime import timedelta
+# delta = timedelta(
+#     days=50,
+#     seconds=27,
+#     microseconds=10,
+#     milliseconds=29000,
+#     minutes=5,
+#     hours=8,
+#     weeks=2
+# )
+# print(delta)
