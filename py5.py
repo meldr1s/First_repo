a31e56e9eaf5d55ad32412e3e3c1e53f0f185d58
meldr1s <vladvip8753@gmail.com>
2025-03-05 21:23:22 +0200

    1

---
+++ py5.py
@@ -0,0 +1,112 @@
+# from collections import namedtuple
+
+# # Створення іменованого кортежу
+# Point = namedtuple('Point', ['x', 'y'])
+
+# p = Point(11, y=22)
+
+# # Доступ до елементів
+# print(p.x)  # 11
+# print(p.y)  # 22
+
+# import collections
+
+# person = ('Mick', 'Nitch', 35, 'Boston', '01146')
+# Person = collections.namedtuple('Person', ['first_name', 'last_name', 'age', 'birth_place', 'post_index'])
+
+# # Створення екземпляра Person
+# person = Person('Mick', 'Nitch', 35, 'Boston', '01146')
+
+# # Виведення різних атрибутів іменованого кортежу
+# print(person.first_name)
+# print(person.post_index)
+# print(person.age)
+# print(person[3])
+
+# Cat = collections.namedtuple('Cat', ['nickname', 'age', 'owner'])
+
+# cat = Cat('Simon', 4, 'Krabat')
+
+# print(f'This is a cat {cat.nickname}, {cat.age} age, his owner {cat.owner}')
+
+# print(f'This is a cat {cat[0]}, {cat[1]} age, his owner {cat[2]}')
+
+# import collections
+
+# student_marks = [4, 2, 4, 6, 7, 4, 2, 3, 4, 5, 6, 6, 7, 1, 1, 1, 3, 5]
+# mark_counts = {}
+# for mark in student_marks:
+#     if mark in mark_counts:
+#         mark_counts[mark] += 1
+#     else:
+#         mark_counts[mark] = 1
+
+# print(mark_counts)
+
+# student_marks = [4, 2, 4, 6, 7, 4, 2, 3, 4, 5, 6, 6, 7 , 1, 1, 1, 3, 5]
+# mark_counts = collections.Counter(student_marks)
+# print(mark_counts)
+# print(mark_counts.most_common())
+# print(mark_counts.most_common(1))
+# print(mark_counts.most_common(2))
+
+# from collections import Counter
+
+# letter_count = Counter("banana")
+# print(letter_count)
+
+# sentence = "the quick brown fox jumps over the lazy dog"
+# words = sentence.split()
+# word_count = Counter(words)
+
+# # Виведення слова та його частоти
+# for word, count in word_count.items():
+#     print(f"{word}: {count}")
+
+# from collections import defaultdict
+
+# # Створення defaultdict з list як фабрикою за замовчуванням
+# d = defaultdict(list)
+
+# # Додавання елементів до списку для кожного ключа
+# d['a'].append(1)
+# d['a'].append(2)
+# d['b'].append(4)
+
+# print(d)
+
+# d = defaultdict(int)
+
+# # Збільшення значення для кожного ключа
+# d['a'] += 1
+# d['b'] += 1
+# d['a'] += 1
+
+# print(d)
+
+# from collections import defaultdict
+
+# words = ['apple', 'zoo', 'lion', 'lama', 'bear', 'bet', 'wolf', 'appendix']
+# grouped_words = {}
+
+# for word in words:
+#     char = word[0]
+#     if char not in grouped_words:
+#         grouped_words[char] = []
+#     grouped_words[char].append(word)
+
+# print(grouped_words)
+
+# if char not in grouped_words:
+#     grouped_words[char] = []
+
+# from collections import defaultdict
+
+# words = ['apple', 'zoo', 'lion', 'lama', 'bear', 'bet', 'wolf', 'appendix']
+# grouped_words = defaultdict(list)
+
+# for word in words:
+#     char = word[0]
+#     grouped_words[char].append(word)
+
+# print(dict(grouped_words))
