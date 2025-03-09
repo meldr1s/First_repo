eae6d48d3d5b0bbc0060b96e9a381e603e12af9f
meldr1s <vladvip8753@gmail.com>
2025-03-09 15:14:53 +0200

    hw4

--- py4hw.py
+++ py4hw.py
@@ -1,85 +1,97 @@
-# def total_salary(path):
-#     try:
-#         total = 0
-#         count = 0
+# /////////// hw4.1////////
+
+
+def total_salary(path):
+    try:
+        total = 0
+        count = 0
         
-#         with open(path, 'r', encoding='utf-8') as file:
-#             for line in file:
-#                 try:
-#                     name, salary = line.strip().split(',')
-#                     total += int(salary)
-#                     count += 1
-#                 except ValueError:
-#                     print(f"Невірний формат даних у рядку: {line}")
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                try:
+                    name, salary = line.strip().split(',')
+                    total += int(salary)
+                    count += 1
+                except ValueError:
+                    print(f"Невірний формат даних у рядку: {line}")
         
-#         if count > 0:
-#             average_salary = total / count
-#         else:
-#             average_salary = 0
+        if count > 0:
+            average_salary = total / count
+        else:
+            average_salary = 0
         
-#         return (total, average_salary)
+        return (total, average_salary)
 
-#     except FileNotFoundError:
-#         print(f"Файл за шляхом {path} не знайдено.")
-#         return (0, 0)
-
-
-# total, average = total_salary(r"C:\Users\ADMIN\Downloads\python\First_repo\path.txt")
-# print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
+    except FileNotFoundError:
+        print(f"Файл за шляхом {path} не знайдено.")
+        return (0, 0)
 
 
-# def get_cats_info(path):
-#     cats_info = []
+total, average = total_salary(r"C:\Users\ADMIN\Downloads\python\First_repo\path.txt")
+print(f"Загальна сума заробітної плати: {total}, Середня заробітна плата: {average}")
 
-#     try:
-#         with open(path, 'r', encoding='utf-8') as file:
-#             for line in file:
-#                 try:
-#                     cat_id, name, age = line.strip().split(',')
-#                     cats_info.append({
-#                         "id": cat_id,
-#                         "name": name,
-#                         "age": age
-#                     })
-#                 except ValueError:
-#                     print(f"Невірний формат даних у рядку: {line}")
+
+# /////////// hw4.2////////
+
+
+def get_cats_info(path):
+    cats_info = []
+
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                try:
+                    cat_id, name, age = line.strip().split(',')
+                    cats_info.append({
+                        "id": cat_id,
+                        "name": name,
+                        "age": age
+                    })
+                except ValueError:
+                    print(f"Невірний формат даних у рядку: {line}")
         
-#         return cats_info
+        return cats_info
 
-#     except FileNotFoundError:
-#         print(f"Файл за шляхом {path} не знайдено.")
-#         return []
+    except FileNotFoundError:
+        print(f"Файл за шляхом {path} не знайдено.")
+        return []
         
-# cats_info = get_cats_info(r"C:\Users\ADMIN\Downloads\python\First_repo\path.txt")
-# print(cats_info)
+cats_info = get_cats_info(r"C:\Users\ADMIN\Downloads\python\First_repo\path.txt")
+print(cats_info)
 
 
-# import os
-# import sys
-# from colorama import Fore, init
+# /////////// hw4.3////////
 
-# init(autoreset=True)
 
-# def list_directory_structure(path):
-#     try:
-#         if not os.path.isdir(path):
-#             print(f"{Fore.RED}Шлях {path} не є директорією або не існує.")
-#             return
+import os
+import sys
+from colorama import Fore, init
 
-#         for root, dirs, files in os.walk(path):
-#             for dir_name in dirs:
-#                 print(f"{Fore.BLUE}{dir_name}")
-#             for file_name in files:
-#                 print(f"{Fore.GREEN}{file_name}")
+init(autoreset=True)
 
-#     except Exception as e:
-#         print(f"{Fore.RED}Помилка: {e}")
+def list_directory_structure(path):
+    try:
+        if not os.path.isdir(path):
+            print(f"{Fore.RED}Шлях {path} не є директорією або не існує.")
+            return
 
-# if len(sys.argv) != 2:
-#     print(f"{Fore.RED}Будь ласка, вкажіть шлях до директорії як аргумент командного рядка.")
-# else:
-#     path = sys.argv[1]
-#     list_directory_structure(path)
+        for root, dirs, files in os.walk(path):
+            for dir_name in dirs:
+                print(f"{Fore.BLUE}{dir_name}")
+            for file_name in files:
+                print(f"{Fore.GREEN}{file_name}")
+
+    except Exception as e:
+        print(f"{Fore.RED}Помилка: {e}")
+
+if len(sys.argv) != 2:
+    print(f"{Fore.RED}Будь ласка, вкажіть шлях до директорії як аргумент командного рядка.")
+else:
+    path = sys.argv[1]
+    list_directory_structure(path)
+
+
+# /////////// hw4.4////////
 
 
 def parse_input(user_input):
